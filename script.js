eae579f05e12d504acd63a166615f9c1debc97bf
Petronilha <petronilhadaniel@gmail.com>
2023-09-18 23:32:13 -0300

    :tada: Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Imagem do Mayk Brito com fundo roxo")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Imagem do Mayk Brito com fundo amarelo")
+  }
+}
