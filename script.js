8a8f9c7915ef2b375697cad3b2af56e5357e9fd4
Petronilha <petronilhadaniel@gmail.com>
2023-09-25 19:41:36 -0300

    Change in nickname and change in stylization

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ function toggleMode() {
   html.classList.toggle("light")
 
   const img = document.querySelector("#profile img")
+  const nickname = document.querySelector("#profile #nickname")
 
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
     img.setAttribute("alt", "Perfil image Daniel Petronilha in light mode")
+    nickname.setAttribute(
+      "src",
+      "https://img.shields.io/badge/-@danielpetronilha-7300FD?style=plastic&logoColor=white"
+    )
+    nickname.setAttribute("alt", "Nickname in light mode")
   } else {
     img.setAttribute("src", "./assets/avatar.png")
     img.setAttribute("alt", "Perfil image Daniel Petronilha in dark mode")
+    nickname.setAttribute(
+      "src",
+      "https://img.shields.io/badge/-@danielpetronilha-EBEBEB?style=plastic&logoColor=white"
+    )
+    nickname.setAttribute("alt", "Nickname in dark mode")
   }
 }
